ba7936d28a91f7d7a50d5b3ebc0a59c8661295ca
akhmadshin <qwasderfasder@gmail.com>
2024-12-26 02:04:06 +0700

    Code quality improve

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-let __RVT_routerKey = 'initial';
-let __RVT_previousRouterKey = undefined;
-let __RVT_transitionElementSelector = undefined;
-let __RVT_transitionAttributeName = undefined;
-let __RVT_transitionAttributeValue = undefined;
+const VIEW_TRANSITION_NAME = '__VTH_transition-name';
+let key = 'initial';
+let previousKey = undefined;
+let transitionEndElementSelector = undefined;
+let transitionEndAttributeName = undefined;
+let transitionEndAttributeValue = undefined;
 
 const cleanUpTransition = () => {
-  const imgElements = Array.from(document.querySelectorAll<HTMLImageElement>(`[style*='view-transition-name']`));
-  const oldTransitionImg = imgElements.find((el) => el.style.viewTransitionName === '__RVT_transition-img');
-  if (oldTransitionImg) {
-    oldTransitionImg.style.viewTransitionName = '';
-  }
+  const viewTransitionElements = Array.from(document.querySelectorAll<HTMLElement>('[style*="view-transition-name: ' + VIEW_TRANSITION_NAME + '"]'));
+  viewTransitionElements.forEach((el) => {
+    el.style.viewTransitionName = '';
+  });
 }
 
 const getElementSelector = (elm: Element) => {
   if (elm.tagName === "BODY") return "BODY";
   const names = [];
-  while (elm.parentElement && elm.tagName !== "BODY") {
+  while (elm.parentElement && elm.tagName.toUpperCase() !== "BODY") {
     if (elm.id) {
       names.unshift("#" + elm.getAttribute("id")); // getAttribute, because `elm.id` could also return a child element with name "id"
       break; // Because ID should be unique, no more is needed. Remove the break, if you always want a full path.
@@ -29,63 +29,72 @@ const getElementSelector = (elm: Element) => {
   return names.join(">");
 }
 
-export const handleHistoryTransitionStarted = (futureKey: string = 'initial') => {
-  const routerKey = __RVT_routerKey ?? 'initial';
-  const bffImgSelector = sessionStorage.getItem(`__RVT_view_transition_image_selector_${routerKey}-${futureKey}`) || '';
-
-  if (bffImgSelector) {
-    const clickedImg = document.querySelector<HTMLImageElement>(bffImgSelector);
-    if (clickedImg && clickedImg.src) {
-      __RVT_transitionAttributeValue = clickedImg.src.replace(location.origin || '', '');
-      cleanUpTransition();
-      clickedImg.style.viewTransitionName = '__RVT_transition-img';
-    }
-  }
-}
-
-export const handleRouteChangeComplete = (_key?: string) => {
-  const key = _key ?? 'initial';
-  __RVT_previousRouterKey = __RVT_routerKey ?? 'initial';
-  __RVT_routerKey = key;
-
-  if (__RVT_transitionElementSelector) {
-    sessionStorage.setItem(`__RVT_view_transition_image_selector_${__RVT_previousRouterKey}-${key}`, __RVT_transitionElementSelector);
-    __RVT_transitionElementSelector = undefined;
-  }
-  // Navigation via back-forward
-  const backRouterKey = `${key}-${__RVT_previousRouterKey}`;
-  const imgSelector = sessionStorage.getItem(`__RVT_view_transition_image_selector_${backRouterKey}`);
-  const img = imgSelector ? document.querySelector<HTMLImageElement>(imgSelector) : undefined;
-
-  cleanUpTransition();
-  if (img) {
-    img.style.viewTransitionName = '__RVT_transition-img';
-  } else {
-    // Navigation via clicking link
-    const transitionImg = document.querySelector<HTMLImageElement>(`[${__RVT_transitionAttributeName}="${__RVT_transitionAttributeValue}"]`);
-    if (transitionImg) {
-      const imgSelector = getElementSelector(transitionImg) || '';
-      transitionImg.style.viewTransitionName = '__RVT_transition-img';
-      sessionStorage.setItem(`__RVT_view_transition_image_selector_${backRouterKey}`, imgSelector);
-    }
-  }
-  __RVT_transitionAttributeValue = undefined;
-}
-
-export const handleTransitionStarted = ({ element, attributeName, attributeValue }: {
+export const handleTransitionStarted = ({ element, attributeName = 'src', attributeValue }: {
   element?: HTMLElement | null;
   attributeName?: string;
   attributeValue: string;
 }) => {
   if (element) {
     const linkSelector = getElementSelector(element);
-    __RVT_transitionElementSelector = linkSelector;
-    __RVT_transitionAttributeValue = attributeValue;
-    __RVT_transitionAttributeName = attributeName;
-    const el = document.querySelector<HTMLImageElement>(`[style*='view-transition-name']`);
-    if (el) {
-      el.style.viewTransitionName = '';
+    transitionEndElementSelector = linkSelector;
+    transitionEndAttributeValue = attributeValue;
+    transitionEndAttributeName = attributeName;
+    cleanUpTransition();
+    element.style.viewTransitionName = VIEW_TRANSITION_NAME;
+  }
+}
+
+export const handleHistoryTransitionStarted = (nextRouterKey: string = 'initial') => {
+  const routerKey = key ?? 'initial';
+  transitionEndAttributeName = undefined;
+  transitionEndAttributeValue = undefined;
+
+  const transitionElementSelector = sessionStorage.getItem(`__VTH_view_transition_element_selector_${routerKey}-${nextRouterKey}`) || '';
+  if (transitionElementSelector) {
+    const transitionElement = document.querySelector<HTMLElement>(transitionElementSelector);
+    if (transitionElement) {
+      cleanUpTransition();
+      transitionElement.style.viewTransitionName = VIEW_TRANSITION_NAME;
     }
-    element.style.viewTransitionName = '__RVT_transition-img';
+  }
+}
+
+export const handleRouteChangeComplete = (_key?: string) => {
+  const routerKey = _key ?? 'initial';
+  previousKey = key ?? 'initial';
+  key = routerKey;
+
+  if (transitionEndElementSelector) {
+    sessionStorage.setItem(`__VTH_view_transition_element_selector_${previousKey}-${key}`, transitionEndElementSelector);
+    transitionEndElementSelector = undefined;
+  }
+  const backRouterKey = `${key}-${previousKey}`;
+  const transitionElementSelector = sessionStorage.getItem(`__VTH_view_transition_element_selector_${backRouterKey}`);
+
+  if (transitionElementSelector) {
+    handleHistoryNavigationComplete(transitionElementSelector);
+  } else {
+    handleLinkNavigationComplete(backRouterKey);
+  }
+
+}
+
+const handleHistoryNavigationComplete = (selector: string) => {
+  const element = document.querySelector<HTMLElement>(selector);
+  if (element) {
+    cleanUpTransition();
+    element.style.viewTransitionName = VIEW_TRANSITION_NAME;
+  }
+}
+
+const handleLinkNavigationComplete = (backRouterKey: string) => {
+  const transitionElement = document.querySelector<HTMLElement>(`[${transitionEndAttributeName}="${transitionEndAttributeValue}"]`);
+  transitionEndAttributeValue = undefined;
+  transitionEndAttributeName = undefined;
+
+  if (transitionElement) {
+    const transitionElementSelector = getElementSelector(transitionElement) || '';
+    transitionElement.style.viewTransitionName = VIEW_TRANSITION_NAME;
+    sessionStorage.setItem(`__VTH_view_transition_element_selector_${backRouterKey}`, transitionElementSelector);
   }
 }
